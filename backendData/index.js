bab0b8793b14561fc43022926ec46091845fd8fd
shwetadubey23 <110589451+shwetadubey23@users.noreply.github.com>
2024-07-28 21:16:57 +0530

    Update index.js

--- backendData/index.js
+++ backendData/index.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
