9b98fdfb87f1290909b7b4ac341c5ec54e3b429d
shwetadubey23 <110589451+shwetadubey23@users.noreply.github.com>
2024-08-01 17:53:48 +0000

    cors issue resolved

--- backendData/index.js
+++ backendData/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./routes/route');
+const cors = require('cors')
 
 const app = express();
 app.use(express.json());
