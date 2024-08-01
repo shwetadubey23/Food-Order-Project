151e83178ea94e05d852afbec4af2a781d645daa
shwetadubey23 <110589451+shwetadubey23@users.noreply.github.com>
2024-08-01 16:09:45 +0000

    production

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -9,7 +9,7 @@ let navigate = useNavigate()
 
 const handleLoginUser = async (eventCheck) => {
   eventCheck.preventDefault();
-  const response = await fetch("https://food-order-project-navy.vercel.app:5000/login", {
+  const response = await fetch("https://food-order-project-navy.vercel.app/login", {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json; charset=utf-8'
