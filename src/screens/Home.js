a9c83819225beee96f4cdb51b046c5b4e12f2318
shwetadubey23 <110589451+shwetadubey23@users.noreply.github.com>
2024-08-02 12:40:31 +0000

    prodction

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -11,8 +11,8 @@ export default function Home() {
     const [foodItem, setFoodItem] = useState([])
 
     const loadData = async () => {
-        let result = await fetch("https://food-order-project-navy.vercel.app/", {
-            method: "GET",
+        let result = await fetch("https://food-order-project-navy.vercel.app/foodData", {
+            method: "POST",
             headers: {
                 'content-type': 'application/json; charset=utf-8'
             }
